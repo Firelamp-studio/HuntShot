cb3a4636f280c857351061a90b7fd059b93be017
Sim1onE <smitarcade@gmail.com>
2021-04-12 19:01:24 +0200

    Imported and updated InputActions API

--- Assets/Game/Scripts/PlayerController.cs
+++ Assets/Game/Scripts/PlayerController.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private PlayerInput playerInput = default;
+
+    private InputActionManager _inputActionManager;
+    private Vector2 _moveDir;
+    private float _rotationValue;
+
     // Start is called before the first frame update
     void Start()
     {
+        _inputActionManager = new InputActionManager(playerInput);
+
+        _inputActionManager.RegisterActionEvent(
+            "Move",
+            InputActionManager.EventType.PERFORMED,
+            ctx => _moveDir = ctx.ReadValue<Vector2>()
+        );
         
+        _inputActionManager.RegisterActionEvent(
+            "Rotate",
+            InputActionManager.EventType.PERFORMED,
+            ctx => _rotationValue = ctx.ReadValue<float>()
+        );
+        
+        _inputActionManager.RegisterActionEvent(
+            "Fire",
+            InputActionManager.EventType.PERFORMED,
+            ctx => OnFire()
+        );
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnFire()
+    {
+        
+    }
+    
+    private void OnDestroy()
+    {
+        _inputActionManager.DisposeAllEvents();
     }
 }
